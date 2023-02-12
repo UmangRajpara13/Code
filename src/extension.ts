c78acec148f46741035f198dfd1924257828b71b
Umang Rajpara <umangrajpara99@gmail.com>
2023-02-12 12:49:01 +0000

    stage,commit,push - vcs support

--- src/extension.ts
+++ src/extension.ts
@@ -1,82 +1,194 @@
-import vscode from 'vscode';
+import { exec, execSync, spawn } from "child_process";
+import { join, relative, sep } from "path";
+import vscode, { commands, Uri, extensions } from "vscode";
 // import { init } from 'vscode-nls-i18n';
 
 // import commands from './commands';
 // import { logger } from './logger';
 
-import { WebSocket } from 'ws';
+import { WebSocket } from "ws";
 
-var defaultPort = process.env.NODE_ENV === 'production' ? 1111 : 2222;
+let some = extensions.getExtension("vscode.git")?.exports;
+// let importedApi = some.exports;
+// console.log(extensions.all)
+console.log(some);
 
+var defaultPort = process.env.NODE_ENV === "production" ? 1111 : 2222;
 
 function sayHello() {
-	vscode.window.showInformationMessage(`Hello Boss [${process.env.NODE_ENV}]`);
-} 
+  vscode.window.showInformationMessage(`Hello Boss`);
+}
 
 export function activate(context: vscode.ExtensionContext): void {
+  console.log("ext Activated!", process.env);
 
-    console.log('ext Activated!',process.env)
+  var isFocused = true;
 
-	var isFocused = true
+  vscode.window.showInformationMessage(
+    `At your service Boss! - Able [${defaultPort} - ${process.env.NODE_ENV}] `
+  );
+  // var myuri: Uri = {
+  //   scheme: "git",
+  //   fsPath: `/home/user/Desktop/My Projects/able_code/.vscode.git`,
+  //   authority: "",
+  //   fragment: "",
+  //   query: "",
+  //   path: `/home/user/Desktop/My Projects/able_code/.vscode.git`,
+  //   with(change) {
+  //     return{
+  //       scheme: "git",
+  //       fsPath: `/home/user/Desktop/My Projects/able_code/.vscode.git`,
+  //       authority: "",
+  //       fragment: "",
+  //       query: "",
+  //       path: `/home/user/Desktop/My Projects/able_code/.vscode.git`,
+  //     }
+  //   },
+  // };
+  // commands.executeCommand(
+  //   "vscode.diff",
+  //   `/home/user/Desktop/My Projects/able_code/.vscode`,
+  //   `/home/user/Desktop/My Projects/able_code/.vscode`
+  // );
+  commands.executeCommand("vscode.git");
 
-	vscode.window.showInformationMessage(`At your service Boss! - Able [${defaultPort}] [${process.env.NODE_ENV}] `);
-	
-	vscode.window.onDidChangeWindowState(winState => {
-		console.log(winState)
-		isFocused = winState.focused
-	})
-	
-    const ws = new WebSocket(`ws://localhost:${defaultPort}`);
-	// console.log(ws, textEditor.document.fileName)
+  vscode.window.onDidChangeWindowState((winState) => {
+    console.log(winState);
+    isFocused = winState.focused;
+  });
 
-	ws.on('error', function error(err) {
-		// vscode.window.showInformationMessage(`[Error] Please check if Schnell is running and listening on ${port}`);
-		console.log('ws error', err);
+  const ws = new WebSocket(`ws://localhost:${defaultPort}`);
+  // console.log(ws, textEditor.document.fileName)
 
-	});
-	ws.on('open', function open() {
-		console.log('ws open');
-		ws.send(`id:code.Code`);
-	});
+  ws.on("error", function error(err) {
+    // vscode.window.showInformationMessage(`[Error] Please check if Schnell is running and listening on ${port}`);
+    console.log("ws error", err);
+  });
+  ws.on("open", function open() {
+    console.log("ws open");
+    ws.send(`id:code.Code`);
+  });
 
-	ws.on('message', function message(data) {
-		console.log('received: %s', data, isFocused);
-		// vscode.window.showInformationMessage(`${data} ${isFocused}`);
+  ws.on("message", function message(data) {
+    console.log("received: %s", data, isFocused);
+    // vscode.window.showInformationMessage(`${data} ${isFocused}`);
 
-		if (!isFocused) return
+    if (!isFocused) return;
 
-		switch (`${data}`) {
-			case `say-hello`:
-				// open vscode new-Window with small size
-				// make terminal fullscreen and add in editor like tabs
-				// remove menubar, activity bar
+    switch (`${data}`) {
+      case `say-hello`:
+        // open vscode new-Window with small size
+        // make terminal fullscreen and add in editor like tabs
+        // remove menubar, activity bar
 
-				sayHello();
-				break;
+        sayHello();
+        break;
+      case `new-window`:
+        exec(`code --new-window`, () => {});
+        break;
 
-			default:
-				break;
-		}
-	});
+      case `audit-changes`:
+      case "git-stage-current-file":
+        var fileName = vscode.window.activeTextEditor?.document.fileName;
 
-	ws.on('close', function message(data) {
-		// vscode.window.showInformationMessage('Schnell : Disconnected!');
-		console.log('ws close');
-	});
+        vscode.window.showInformationMessage(
+          `Stagging -> ${fileName?.substring(fileName.lastIndexOf(sep) + 1)}`
+        );
 
-    // init(context.extensionPath);
-    // console.log(process.env.NODE_ENV)
-    // logger.log(`language: ${vscode.env.language}`);
-    // const { remoteName } = vscode.env;
-    // if (remoteName) {
-    //     logger.log(`active extension in ${remoteName} remote environment`);
-    // }
+        var projectRoot;
 
-    // commands.forEach((command) => {
-    //     context.subscriptions.push(vscode.commands.registerCommand(command.identifier!, command.handler));
-    // });
+        vscode.workspace.workspaceFolders?.forEach((folder) => {
+          if (fileName?.includes(folder.uri.fsPath)) {
+            projectRoot = folder.uri.fsPath;
+          }
+        });
+
+        console.log(
+          process.cwd(),
+          projectRoot,
+          vscode.workspace.workspaceFolders,
+          vscode.window.activeTextEditor
+        );
+
+        try {
+          execSync(`cd '${projectRoot}' && git add '${fileName}'`);
+          // var execute = spawn("git", ["add", `${fileName}`], {
+          //   cwd: projectRoot,
+          // });
+          // execute.stdout.on("data", (data) => {
+          //   // process.stdout.write(`<< ${data} `.replace("\n", ""));
+          // });
+
+          // execute.stderr.on("data", (data) => {
+          //   // console.error(`stderr: ${data}`);
+          // });
+
+          // execute.on("close", (code) => {
+          //   // eslint-disable-next-line eqeqeq
+          //   if (code == 0) {
+          //     vscode.commands.executeCommand(
+          //       "workbench.action.closeActiveEditor"
+          //     );
+          //     return;
+          //   }
+          //   process.stdout.write(`Exit Code ${code}`);
+          // });
+          vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+        } catch (error) {
+          vscode.window.showWarningMessage(`${error}`);
+        }
+        break;
+
+      case "git-commit":
+        vscode.window.showInformationMessage("commit");
+
+        // open input box
+        vscode.window
+          .showInputBox({ placeHolder: "Enter a Commit Message" })
+          .then((value) => {
+            console.log(value);
+            // eslint-disable-next-line curly
+            if (value === undefined) return;
+
+            var projectRoot = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+
+            execSync(`cd '${projectRoot}' && git commit -m "${value}"`);
+          });
+
+        break;
+
+      case "git-push":
+        projectRoot = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+        try {
+          execSync(`cd '${projectRoot}' && git push`);
+          vscode.window.showInformationMessage("Push Successfull");
+        } catch (error) {
+          vscode.window.showInformationMessage(`${error}`);
+        }
+        break;
+      default:
+        break;
+    }
+  });
+
+  ws.on("close", function message(data) {
+    // vscode.window.showInformationMessage('Schnell : Disconnected!');
+    console.log("ws close");
+  });
+
+  // init(context.extensionPath);
+  // console.log(process.env.NODE_ENV)
+  // logger.log(`language: ${vscode.env.language}`);
+  // const { remoteName } = vscode.env;
+  // if (remoteName) {
+  //     logger.log(`active extension in ${remoteName} remote environment`);
+  // }
+
+  // commands.forEach((command) => {
+  //     context.subscriptions.push(vscode.commands.registerCommand(command.identifier!, command.handler));
+  // });
 }
 
 export function deactivate(): void {
-    // logger.dispose();
+  // logger.dispose();
 }
